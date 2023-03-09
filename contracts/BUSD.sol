11f533989dac27663c63958723c94ef7acc59d77
rishal.ep <rishal.ep@accubits.com>
2023-03-09 13:51:32 +0530

    conttracts added

---
+++ contracts/BUSD.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BUSD is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 500000 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
