bb34b887f1b8fa78ababf97e600a718785db7827
rishal.ep <rishal.ep@accubits.com>
2023-05-21 18:54:54 +0530

    contract updates

--- contracts/TenXv1.sol
+++ contracts/TenXv1.sol
@@ -1,87 +1,224 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@chainlink/contracts-upgradeable/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract TenxUpgradable is Initializable, OwnableUpgradeable {
+/**
+ * @author  Rishal EP
+ * @title   Tenx Subscription contract
+ * @dev     This contract is used for splitting 
+ *          users subscribtions to affiliates and shareholders
+ */
+
+contract TenxUpgradableV1 is Initializable, AccessControlUpgradeable, PausableUpgradeable {
+
     using SafeERC20Upgradeable for IERC20Upgradeable;
+    using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    // Add your contract variables and functions here
-    function initialize(
-        address[] memory _shareHolderWallet,
-        uint256[] memory _shareHolderPercentage,
-        address _reinvestmentWallet,
-        uint256 _referralLevel,
-        uint256[] memory _referralPercentage,
-        uint256[] memory months,
-        uint256[] memory pricing
-    ) external virtual initializer {
-        __Ownable_init();
-    }
-
-    uint256 public totalReferralIds;
-    uint256 public BNBFromFailedTransfers; // BNB left in the contract from failed transfers
-
-    uint256 public referralLevels;
-    address public reInvestmentWallet;
-    address[] public shareHolderWallet;
-    uint256[] public shareHolderPercentage; // percentage should whithout decimal EX:- 33.75 will like 3375
-    mapping(uint256 => uint256) public referralPercentage;
-
-    struct SubscriptionPlan {
+    /**
+     * @dev Struct defining the parameters of a subscribtion scheme.
+     * @param price is the dollar price for this subscribtion.
+     * @param plan is the subscribtion plan (duration) in seconds.
+     * @param active is a boolean indicating whether this scheme is currently active or not.
+     */
+    struct SubscribtionScheme {
         uint256 price;
-        uint256 duration;
-        bool exist;
+        uint256 lockingPeriod;
+        bool active;
     }
-    mapping(uint256 => SubscriptionPlan) public subscriptionPlans; // months -> plan details
 
+    /**
+     * @dev Struct representing data associated with a particular user.
+     * @param referralId is users unique id.
+     * @param plan is the subscribtion plan (duration) in seconds.
+     * @param active is a boolean indicating whether this scheme is currently active or not.
+     */
     struct User {
         uint256 referralId;
         uint256 referredBy;
         uint256 subscriptionValidity;
     }
-    mapping(address => User) public users; // user address -> User
-    mapping(uint256 => address) public referralIdToUser; // referralId -> user address
 
+    /**
+     * @dev Struct representing data associated with a payment token.
+     * @param priceFeed is price feed address of the token.
+     * @param active is a boolean indicating whether this scheme is currently active or not.
+     */
     struct PaymentToken {
-        address priceFeed; // Chainlink price feed
-        bool exist;
+        address priceFeed;
+        bool active;
     }
-    mapping(address => PaymentToken) public paymentTokens;
 
-     /* Events */
-    event TransferOfBNBFail(address indexed receiver, uint256 indexed amount);
-    event SetShareHolder(address indexed shareHolderWallet, uint128 index);
-    event SetShareHolderPercentage(
-        uint256 indexed shareHolderPercentage,
-        uint128 index
-    );
+     /**
+     * @dev Struct representing each shareholder details.
+     * @param name is name of share holder.
+     * @param holderAddress is address of share holder.
+     * @param sharePercentage is percantage share for share holder
+     * @param active is a boolean indicating whether this holder is currently active or not.
+     */
+    struct ShareHolder {
+        string name;
+        address holderAddress;
+        uint256 sharePercentage;
+        bool active;
+    }
 
-    event SetReferralPercentage(uint256 referralPercentage, uint128 index);
-    event SetReInvestmentWallet(address indexed reInvestmentWallet);
+    /**
+     * @dev Struct representing shareholder and affiliates detail in common.
+     * @param totalLevels is the total number of share holders/affiliates.
+     * @param totalShare is total percantage of share for all the shareholders/affiliates.
+     */
+    struct Shares {
+        uint256 totalLevels;
+        uint256 totalShare;
+    }
 
-    event CreateUser(
-        address indexed user,
-        uint256 indexed referralId,
-        uint256 indexed referredBy
-    );
+    /// Constant representing the manager role.
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
-    event Subscription(
-        uint256 amount,
-        uint120 period,
-        address indexed subscriber,
-        address paymentToken
-    );
-    event AddPaymentToken(address indexed paymentToken);
-    event RemovePaymentToken(address indexed paymentToken);
+    /// Variable to store failed transaction BNB.
+    uint256 internal BNBFromFailedTransfers_;
+
+    /// Counter to keep track of the total users.
+    CountersUpgradeable.Counter internal userID_;
+    
+    /// Variable to store re investment wallet.
+    address internal reInvestmentWallet_;
+    
+    /// Variable to store total referal levels.
+    address internal referralLevels_;
+
+    /// Variable to store total shareholder levels.
+    address internal totalShareHolders_;
+
+    /// Mapping to store all the users created.
+    mapping(uint256 => User) internal users_;
+    
+    /// Mapping to payment all the payment tokens created.
+    mapping(address => PaymentToken) internal paymentTokens_;
+
+    /// Mapping to save all share holders details.
+    mapping(uint256 => ShareHolder) internal shareHolders_;
+
+    /// Mapping to store all the schemes created.
+    mapping(uint256 => SubscribtionScheme) internal subscribtionSchemes_;
+    
+    /// Variable store the referal level info in detail.
+    Shares internal referalShares_
+
+    /// Variable store the shareHolder info in detail.
+    Shares internal holderShares_
+
+    // Add Events Here
+
+    /**
+     * @dev Initializes the contract.
+     * This function is used to set initial values for certain variables/parameters when the contract is first deployed.
+     */
+    function initialize(Shares _shareHolderDetail, Shares _referralDetail) external initializer {
+        __Pausable_init();
+        __AccessControl_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _grantRole(MANAGER_ROLE, _msgSender());
+
+        _setShareHolderDetail(_shareHolderDetail);
+        _setReferalLevelDetail(_referralDetail);
+    }
+
+    /**
+     * @dev Pauses the contract, disabling interaction with it.
+     *
+     * Requirements:
+     * - Only the admin or manager can call this function.
+     */
+    function pause() external isManager {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses the contract, allowing interaction with it.
+     *
+     * Requirements:
+     * - Only the admin or manager can call this function.
+     */
+    function unpause() external isManager {
+        _unpause();
+    }
+
+
+    /**
+     * @dev Updates the Total Share holder Levels and Percentage.
+     * @param _shareHolderDetail The Share Holders Details to be updated.   
+     */
+    function setShareHolders(
+        string[] memory _name, 
+        address[] _userAddress, 
+        uint256[] _sharePercant
+    ) external isManager {
+        require(
+            _name.length === 
+            _userAddress.length === 
+            _sharePercant.length === 
+            holderShares_.totalLevels,
+            "Tenx: Share Holder Level Mismsatch in Inputs"
+        );
+
+        
+
+    }
 
 
 
 
+
+
+
+
+
+
+
+    /**
+     * @dev Updates the Total Share holder Levels and Percentage.
+     * @param _shareHolderDetail The Share Holders Details to be updated.   
+     */
+    function _setShareHolderDetail(Shares _shareHolderDetail) internal {
+        require(
+            _shareHolderDetail.totalShare > 0 &&
+                _shareHolderDetail.totalShare < 10000,
+            "Tenx: Total Percentage should be betweeen 0 to 100"
+        );
+
+        require(
+            _shareHolderDetail.totalLevels > 0,
+            "Tenx: Total Levels should be non zero"
+        );
+
+       holderShares_ = _shareHolderDetail
+    }
+
+    /**
+     * @dev Updates the Total referral Levels and Percentage.
+     * @param _referralDetail The Share Holders Details to be updated.   
+     */
+    function _setReferalLevelDetail(Shares _referralDetail) internal {
+        require(
+            _referralDetail.totalShare > 0 &&
+                _referralDetail.totalShare < 10000,
+            "Tenx: Total Percentage should be betweeen 0 to 100"
+        );
+
+        require(
+            _referralDetail.totalLevels > 0,
+            "Tenx: Total Levels should be non zero"
+        );
+
+       referalShares_ = _referralDetail
+    }
 
 }
