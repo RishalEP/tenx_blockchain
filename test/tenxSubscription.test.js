24bb4452135fd1b96ef4899a55b2d7c5a31c88be
rishal.ep <rishal.ep@accubits.com>
2023-05-26 14:35:35 +0530

    bug fix

--- test/tenxSubscription.test.js
+++ test/tenxSubscription.test.js
@@ -1,6 +1,5 @@
 const {
-    loadFixture,
-    time
+    loadFixture
   } = require("@nomicfoundation/hardhat-network-helpers");
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
