7b914523f6539366bdfac06391381a2be440899c
rishal.ep <rishal.ep@accubits.com>
2023-06-13 17:34:49 +0530

    script updates

--- scripts/3-deployUsdt.js
+++ scripts/3-deployUsdt.js
@@ -5,12 +5,12 @@ const saveToConfig = require('../utils/saveToConfig')
 async function main() {
 
         const USDT = await ethers.getContractFactory("ERC20Token");
-        const ERC20TokenABI = (await artifacts.readArtifact('BUSD')).abi
+        const ERC20TokenABI = (await artifacts.readArtifact('ERC20Token')).abi
         await saveToConfig('ERC20', 'ABI', ERC20TokenABI)
-        const busd = await USDT.deploy('USDT Token', 'USDT');
-        await saveToConfig('USDT', 'ADDRESS', USDT.address)
+        const usdt = await USDT.deploy('USDT Token', 'USDT');
+        await saveToConfig('USDT', 'ADDRESS', usdt.address)
         await saveToConfig('USDT', 'CHAINID', hre.network.config.chainId)
-        console.log(`USDT:- ${USDT.address} `);
+        console.log(`USDT:- ${usdt.address} `);
 
 }
 
